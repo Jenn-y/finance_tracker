bf431b64578e60d25770b1aa8bacf9a4c452dc03
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-09-13 15:31:03 +0200

    added tabular view of the results

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times do |user|
+  User.create!(
+    email: "user#{user}@gmail.com",
+    password: '123456'
+  )
+end
+
+3.times do |stock|
+  Stock.create!(
+    ticker: "AAPL",
+    name: 'Apple inc.',
+    last_price: 222
+  )
+end
+
+3.times do |user_stock|
+  UserStock.create!(
+    user_id: User.first.id,
+    stock_id: Stock.first.id
+  )
+end
